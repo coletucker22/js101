2a345acda79c7ab77aff366e8739daef55b91211
coletucker22 <59104432+coletucker22@users.noreply.github.com>
2020-01-30 19:06:37 -0600

    Completed

--- exercises/106-math.js
+++ exercises/106-math.js
@@ -7,30 +7,43 @@
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Create a function "add99" that takes one argument and adds the number 99 to it.
 // You can assume that the argument passed in will be a number value.
-
+function add99 () {
+    return (argument[0] + 99)
+}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Create a function "sum" that takes 2 arguments and sums them together.
 // Assume that both arguments are numbers.
-
+function sum () {
+    return (argument[0] + argument[1])
+}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Create a function "difference" that takes 2 number arguments and returns their
 // difference.
 // ie: the second number substracted from the first number
-
+function difference () {
+    return (argument[0] - argument[1])
+}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Create a function "multiply" that takes 2 number arguments and returns their
 // product.
-
+function multiply () {
+    return (argument[0] * argument[1])
+}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Create a function "divide" that takes 2 number arguments and returns the
 // division of the first argument by the second.
-
+function divide () {
+    return (argument[0] / argument[1])
+}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // The modulus operator (%) works like remainder from division.
 // Create a function "mod" that takes 2 number arguments and returns the
 // remainder of the first divided by the second.
+function mod () {
+    return (argument[0] / argument[1])
+}
